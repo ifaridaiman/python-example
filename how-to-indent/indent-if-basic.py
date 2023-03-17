679b2b490e37db4cb47c98d7a16578341ebb54f4
Red <faiman@esrimalaysia.com.my>
2023-03-17 10:34:30 +0800

    add one more indent basic sample to show how to indent

---
+++ how-to-indent/indent-if-basic.py
@@ -0,0 +1,10 @@
+department_a = 18
+department_b = 20
+department_c = 30
+
+if(department_a >= 15):
+    print("department_a is more than 15 staff")
+    if(department_b <30):
+        print("department b is less than 30 staff")
+else:
+    print("department_a  is less than 15")
